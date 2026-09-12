e1854df9edbe54f2fe5f592c2e91cbe16f25a19f
Karl Kreutzfeldt <kkb.reach@protonmail.com>
2026-09-12 11:30:38 +0200

    Add files via upload

--- api/quotes.js
+++ api/quotes.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Cache-Control', 's-maxage=30, stale-while-revalidate=60');
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
